36bc40b19d04940f679ebb0782fa6fbde80254bf
hogiabao <hogiabao7725@gmail.com>
2026-07-06 21:14:18 +0700

    chore: init project structure with spring boot and react ts

---
+++ backend/src/test/java/com/hogiabao7725/hotelbooking/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hogiabao7725.hotelbooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
