08e863d8ab99ec88ef5ee1f3f0eac3ffce95328b
hogiabao <hogiabao7725@gmail.com>
2026-07-16 09:10:23 +0700

    feat(security): integrate custom entrypoint and access denied handler

--- backend/src/main/java/com/hogiabao7725/hotelbooking/security/config/SecurityConfig.java
+++ backend/src/main/java/com/hogiabao7725/hotelbooking/security/config/SecurityConfig.java
@@ -1,17 +1,28 @@
 package com.hogiabao7725.hotelbooking.security.config;
 
+import com.hogiabao7725.hotelbooking.security.handler.CustomAccessDeniedHandler;
+import com.hogiabao7725.hotelbooking.security.handler.CustomAuthenticationEntrypoint;
+import com.hogiabao7725.hotelbooking.security.jwt.JwtAuthenticationFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    private final CustomAuthenticationEntrypoint authenticationEntrypoint;
+    private final CustomAccessDeniedHandler accessDeniedHandler;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -27,10 +38,20 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) {
         http
                 .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+
+                .exceptionHandling(ex -> ex
+                        .authenticationEntryPoint(authenticationEntrypoint)
+                        .accessDeniedHandler(accessDeniedHandler))
+
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(SecurityEndpoints.PUBLIC_ENDPOINTS).permitAll()
                         .anyRequest().authenticated()
                 )
+
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+
                 .formLogin(AbstractHttpConfigurer::disable)
                 .httpBasic(AbstractHttpConfigurer::disable);
 
--- backend/src/main/java/com/hogiabao7725/hotelbooking/security/handler/CustomAuthenticationEntrypoint.java
+++ backend/src/main/java/com/hogiabao7725/hotelbooking/security/handler/CustomAuthenticationEntrypoint.java
@@ -5,24 +5,24 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.security.access.AccessDeniedException;
-import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerExceptionResolver;
 
 @Component
 @RequiredArgsConstructor
-public class CustomAccessDeniedHandler implements AccessDeniedHandler {
+public class CustomAuthenticationEntrypoint implements AuthenticationEntryPoint {
 
     @Qualifier("handlerExceptionResolver")
     private final HandlerExceptionResolver handlerExceptionResolver;
 
     @Override
-    public void handle(
+    public void commence(
             @NonNull HttpServletRequest request,
             @NonNull HttpServletResponse response,
-            @NonNull AccessDeniedException accessDeniedException
+            @NonNull AuthenticationException authException
     ) {
-        handlerExceptionResolver.resolveException(request, response, null, accessDeniedException);
+        handlerExceptionResolver.resolveException(request, response, null, authException);
     }
 }
